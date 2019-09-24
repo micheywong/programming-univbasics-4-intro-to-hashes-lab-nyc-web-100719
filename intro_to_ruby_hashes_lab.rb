c24f65d45f753130921f186ba878844deabd6fd7
Michelle Wong <michellekwong2@gmail.com>
2019-09-24 14:20:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,7 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
   update_counting_hash = {
-    if ["top hat"]
+    if :apples => 
       update_counting_hash["top hat"] += 1
     else
     update_counting_hash["top hat"] = 1
